9fb0bdc490483c684b6dd19cce8c5da28d52302a
juanchidurini <jdurini@udesa.edu.ar>
2025-11-03 18:10:46 -0300

    arregle logout

--- proyectof/src/screens/Profile.js
+++ proyectof/src/screens/Profile.js
@@ -1,15 +1,22 @@
 import { Text, View, Pressable } from 'react-native'
 import React, { Component } from 'react'
+import { auth } from '../firebase/config';
 
 export class Profile extends Component {
   constructor(props) {  
     super(props)
   }
+
+  manejarLogout() {
+    auth.signOut()
+    this.props.navigation.navigate('Login');
+  }
+
   render() {
     return (
       <View>
         <Text>Profile</Text>
-        <Pressable onPress={() => this.props.navigation.navigate('Login')}>
+        <Pressable onPress={() => this.manejarLogout()}>
           <Text>
             Logout
           </Text>
