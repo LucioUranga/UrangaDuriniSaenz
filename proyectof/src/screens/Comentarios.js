f673181229e0f4bc2a161fe51321771b00c40e61
LucioUranga <lurangawhite@udesa.edu.ar>
2025-11-10 16:48:19 -0300

    Comentarios

--- proyectof/src/screens/Comentarios.js
+++ proyectof/src/screens/Comentarios.js
@@ -1,45 +1,71 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, Pressable, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Pressable, FlatList, StyleSheet } from 'react-native';
 import { db, auth } from '../firebase/config';
-
+import firebase from 'firebase/app';
 
 export class Comentarios extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      comentarios: []
+      comentario: '',
+      comentarios: [],
+      postId: this.props.route.params.postId
     };
   }
-componentDidMount() {
-    
+
+  componentDidMount() {
     db.collection('posts')
-      .doc(this.props.route.params.postId)
+      .doc(this.state.postId)
       .onSnapshot((doc) => {
-        const data = doc.data();
-        this.setState({ comentarios: data.comments || [] });
+        this.setState({ comentarios: doc.data().comentarios || [] });
       });
+  }
+
+  handleComment() {
+    const user = auth.currentUser.email;
+    db.collection('posts')
+      .doc(this.state.postId)
+      .update({
+        comentarios: firebase.firestore.FieldValue.arrayUnion({
+          user: user,
+          text: this.state.comentario,
+          createdAt: Date.now(),
+        }),
+      })
+      .then(() => this.setState({ comentario: '' }));
   }
 
   render() {
     return (
-      <View>
-        <Text>Crear nuevo comentario</Text>
-
-        <TextInput
-          placeholder="Escribe aquí tu comentario..."
-          onChangeText={(text) => this.setState({ description: text })}
-          value={this.state.comentarios}
+      <View style={styles.container}>
+        <Text style={styles.title}>Comentarios</Text>
+        <FlatList
+          data={this.state.comentarios}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <Text>{item.user}: {item.text}</Text>
+          )}
         />
-
-        <Pressable onPress={() => this.handlePost()}>
-          <Text>Publicar comentario</Text>
+        <TextInput
+          placeholder="Escribe un comentario..."
+          value={this.state.comentario}
+          onChangeText={(text) => this.setState({ comentario: text })}
+          style={styles.input}
+        />
+        <Pressable onPress={() => this.handleComment()}>
+          <Text style={styles.button}>Agregar comentario</Text>
         </Pressable>
-
-
       </View>
     );
   }
 }
 
-export default Comentarios
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 10 },
+  title: { fontWeight: 'bold', fontSize: 18, marginBottom: 10 },
+  input: { borderWidth: 1, borderColor: '#ccc', padding: 8, marginTop: 10 },
+  button: { color: 'blue', marginTop: 10 },
+});
 
+
+export default Comentarios;
